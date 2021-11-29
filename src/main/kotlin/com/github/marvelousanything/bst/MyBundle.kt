4e002c8b6bc953557bae55c0f4778961bb308778
GitHub Action <action@github.com>
2021-11-29 21:33:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/marvelousanything/bst/MyBundle.kt
+++ src/main/kotlin/com/github/marvelousanything/bst/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.marvelousanything.bst
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/marvelousanything/bst/services/MyProjectService.kt
+++ src/main/kotlin/com/github/marvelousanything/bst/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.marvelousanything.bst.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.marvelousanything.bst.MyBundle
 
 class MyProjectService(project: Project) {
 
